d2afb4847d07f109d8d55d03b64de95928839241
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-02 13:19:59 +0530

    MySQLConvert

--- passwd.inc.php
+++ passwd.inc.php
@@ -1,8 +1,8 @@
 <?php
 // Configuration File of Administration
-// Generated: Wed, 22 Jul 2009 14:20:59 +0200
+// Generated: Mon, 02 Nov 2020 07:38:06 +0100
 
-$pmp_admin = "";
-$pmp_passwd = "";
+$pmp_admin = 'admin';
+$pmp_passwd = '$2y$10$Y4.TLLm/D.nFT95LELefpOE8wci6sq7mJ7Elpjwl/GPXwbQ0e5Mca';
 
 ?>
